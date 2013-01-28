e0e206d8297a159742ea95fff1118bdab7d82881
Naftoli Gugenheim <naftoligug@gmail.com>
2013-01-28 05:46:07 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "scheduler"
+
+version := "1.1"
+
+organization := "nafg"
+
+scalaVersion := "2.10.0-RC2"
+
+scalaBinaryVersion <<= scalaVersion
+
---
+++ src/main/scala/scheduler/Schedulable.scala
@@ -0,0 +1,25 @@
+package scheduler
+
+trait CanSchedule[T] {
+  type Task = T
+  def forAllDue(f: Task => Unit): Unit
+  def execute(t: Task): Unit
+  def nextTime(t: Task): Long
+  def delete(t: Task): Unit
+}
+
+class Scheduler {
+  private var canSchedules = List[CanSchedule[_]]()
+
+  def addTask[T](t: T)(implicit view: T => CanSchedule[T]): Unit = addTask(view(t))
+  def addTask[T](implicit canSchedule: CanSchedule[T]): Unit = synchronized {
+    canSchedules ::= canSchedule
+  }
+
+  def run() = canSchedules foreach { cs =>
+    cs forAllDue { t: cs.Task =>
+      cs execute t
+      if (cs.nextTime(t) < System.currentTimeMillis) cs delete t
+    }
+  }
+}
